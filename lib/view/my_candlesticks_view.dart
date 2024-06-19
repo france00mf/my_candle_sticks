b2170268353dbc191c95b62e1f03bf5e128b29f3
France! <franciscodomingosjose06798@gmail.com>
2024-06-19 17:55:34 +0100

    create candles model and make some updates

--- lib/view/my_candlesticks_view.dart
+++ lib/view/my_candlesticks_view.dart
@@ -1,4 +1,5 @@
 
+import 'package:candlesticks/candlesticks.dart';
 import 'package:flutter/material.dart';
 
 class MyCandleSticksView extends StatefulWidget {
@@ -11,6 +12,19 @@ class MyCandleSticksView extends StatefulWidget {
 class _MyCandleSticksViewState extends State<MyCandleSticksView> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SingleChildScrollView(
+      physics: BouncingScrollPhysics(),
+      child: Column(
+        children: [
+          SizedBox(
+            height: 400,
+            width: double.infinity,
+            child: Candlesticks(
+              key: Key(),
+              candles: candles),
+          )
+        ],
+      ),
+    );
   }
 }
