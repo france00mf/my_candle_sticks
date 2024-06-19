7c68d3698130f21c54143321242ae07e34d2d06a
France! <franciscodomingosjose06798@gmail.com>
2024-06-19 17:50:43 +0100

    add mvc struck pattern

---
+++ lib/view/my_candlesticks_view.dart
@@ -0,0 +1,16 @@
+
+import 'package:flutter/material.dart';
+
+class MyCandleSticksView extends StatefulWidget {
+  const MyCandleSticksView({super.key});
+
+  @override
+  State<MyCandleSticksView> createState() => _MyCandleSticksViewState();
+}
+
+class _MyCandleSticksViewState extends State<MyCandleSticksView> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
